b8f86801b3d8bdee597d8efa8d9bd08ac296aa23
Shivpvtltd <kumawatsurendra945@gmail.com>
2026-03-02 13:11:09 +0530

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,7 +6,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const { getFirestore } = require('../utils/firestore');
+const { getFirestore } = require('../utils/firebase');
 const axios = require('axios');
 
 // Configure Google OAuth Strategy
